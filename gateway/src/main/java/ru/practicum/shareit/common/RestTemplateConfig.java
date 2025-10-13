32ac92b2a958f09a475080e5d7c9348b01b8bcfb
Aleksey Seledcov <alex-ozu@yandex.ru>
2025-10-13 17:30:52 +0300

    Adding tests

---
+++ gateway/src/main/java/ru/practicum/shareit/common/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.common;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplateBuilder restTemplateBuilder() {
+        return new RestTemplateBuilder();
+    }
+}
